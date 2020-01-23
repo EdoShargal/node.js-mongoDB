574ac6958dd93c3cc6903896f77d3544a6650bc5
edo shargal <edosh528@gmail.com>
2020-01-23 18:13:30 +0200

    first upload

---
+++ api/routes/products.js
@@ -0,0 +1,134 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const router = express.Router()
+
+
+const Product = require('../models/product')
+
+router.get('/', (req, res, next) => {
+    Product.find()
+        .select("name price _id")
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                product: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:800/products/' + doc._id
+                        }
+                    }
+                })
+            }
+            // if(docs.length > 0) {
+              res.status(200).json(response)
+            // }
+            // else res.status(404).json({
+            //     message: 'No entries found'
+            // })
+        })
+        .catch(err => {
+            console.log(err)
+            tus(500).json({ error: err })
+        })
+})
+
+router.post('/', (req, res, next) => {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // }
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    })
+    //product.save((err, result) => { doing something})
+    product.save().then(result => {
+        console.log(result)
+        res.status(201).json({
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:8000/products" + result._id
+            }
+        })
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: err })
+    })
+
+})
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From database", doc)
+            if (doc) {
+                res.status(200).json(doc)
+            } else {
+                res.status(404).json({ message: 'No valid entry found for provided ID' })
+            }
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err })
+        })
+    // if (id === 'special'){
+    //     res.status(200).json({
+    //         message: 'You discovered the special ID',
+    //         id: id
+    //     })
+    // } else {
+    //     res.status(200).json({
+    //         message: 'You passed an ID'
+    //     })
+    // }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    const updateOps = {}
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id: id}, {$set: updateOps})
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const productRoutes = require('./api/routes/products')
+const orderRoutes = require('./api/routes/orders')
+
+// add mongoose helper more mongodb
+mongoose.connect(`mongodb+srv://adminedo:${process.env.MONGO_ATLAS_PW}@node-rest-api-dtdwu.mongodb.net/test?retryWrites=true&w=majority`, 
+                { 
+                    // for under the hood it will use the MongoDB client for
+                    //useMongoClient: true 
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                })
+// loging the calling route to console 
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false})) // extended: false - simple bodies for URL encoded data
+app.use(bodyParser.json())
+
+// Enabled cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access=Control-Allow-Metgods', 'PUT, POST, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+// routes url
+app.use('/products', productRoutes)
+app.use('/orders', orderRoutes)
+
+
+// Handling Errors
+
+// error if no macth for route url
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+// error for all the rest 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app
